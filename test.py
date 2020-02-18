535606c08988603ac89475522cc3d3e0de61d887
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-18 17:28:37 +0900

    Update test.py

--- test.py
+++ test.py
@@ -63,7 +63,7 @@ async def on_message(message):
         SearchID = message.content[len('!재고')+1:]
         gc = gspread.authorize(creds)
         wks = gc.open('GJ재고관리').worksheet('영업재고출력')
-        wkstime = gc.open('GJ재고관리').worksheet('데이터')
+        wkstime = gc.open('GJ재고관리').worksheet('재고데이터')
         wks.update_acell('A1', SearchID)
         result = wks.acell('B1').value
         result2 = wkstime.acell('A1').value
